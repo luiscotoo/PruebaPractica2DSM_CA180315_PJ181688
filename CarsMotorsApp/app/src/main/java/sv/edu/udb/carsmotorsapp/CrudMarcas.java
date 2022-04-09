f5e84aa86ec20b27a9af6743716557c3fb253e96
Luis Coto <luiscoto001@gmail.com>
2022-04-09 09:50:40 -0600

    Modificacion Crud Marcas

--- CarsMotorsApp/app/src/main/java/sv/edu/udb/carsmotorsapp/CrudMarcas.java
+++ CarsMotorsApp/app/src/main/java/sv/edu/udb/carsmotorsapp/CrudMarcas.java
@@ -13,8 +13,7 @@ import android.widget.Toast;
 
 public class CrudMarcas extends AppCompatActivity {
 
-    private EditText etMarca;
-    private TextView tvCodigo;
+    private EditText etMarca, etIdMarca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,62 +21,87 @@ public class CrudMarcas extends AppCompatActivity {
         setContentView(R.layout.activity_crud_marcas);
 
         etMarca = (EditText) findViewById(R.id.etMarca);
-        tvCodigo=(TextView) findViewById(R.id.tvCodigo);
+        etIdMarca = (EditText) findViewById(R.id.etIdMarca);
     }
     public void Ingresar(View v){
-    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
+    AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"CarsMotorsDB", null, 1);
         SQLiteDatabase bd= admin.getWritableDatabase();
+        String id = etIdMarca.getText().toString();
         String Nombre=etMarca.getText().toString();
-        ContentValues registro=new ContentValues();
-        registro.put("nombre",Nombre);
-        bd.insert("marcas",null,registro);
+        ContentValues registro=new ContentValues();;
+        if(Nombre.isEmpty()){
+            Toast.makeText(this,"Ingrese un nombre",Toast.LENGTH_SHORT).show();
+        }else{
+            registro.put("nombre",Nombre);
+            bd.insert("marcas",null,registro);
+            etMarca.setText("");
+            etIdMarca.setText("");
+            Toast.makeText(this,"Se ingreso la Marca", Toast.LENGTH_SHORT).show();
+
+        }
         bd.close();
-        etMarca.setText("");
-        tvCodigo.setText("");
-        Toast.makeText(this,"Se ingreso la Marca", Toast.LENGTH_SHORT).show();
+
     }
     public void Buscar(View v){
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"CarsMotorsDB", null, 1);
         SQLiteDatabase bd= admin.getWritableDatabase();
         String Nombre=etMarca.getText().toString();
-        Cursor fila=bd.rawQuery("select idmarcas,nombre from marcas where nombre='"+Nombre+"'",null);
-        if (fila.moveToFirst()){
-            tvCodigo.setText("ID: "+fila.getString(0));
-            etMarca.setText(fila.getString(1));
-            Toast.makeText(this,"Marca Encontrada con exito",Toast.LENGTH_SHORT).show();
+        if(Nombre.isEmpty()){
+            Toast.makeText(this,"Ingrese un nombre",Toast.LENGTH_SHORT).show();
+        }else{
+            Cursor fila=bd.rawQuery("select idmarcas,nombre from marcas where nombre='"+Nombre+"'",null);
+            if (fila.moveToFirst()){
+                etIdMarca.setText(fila.getString(0));
+                etMarca.setText(fila.getString(1));
+                Toast.makeText(this,"Marca Encontrada con exito",Toast.LENGTH_SHORT).show();
+            }
+            else
+                Toast.makeText(this,"No existe dicha marca",Toast.LENGTH_SHORT).show();
         }
-        else
-            Toast.makeText(this,"No existe dicha marca",Toast.LENGTH_SHORT).show();
         bd.close();
     }
 
     public void Actualizar(View V){
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"CarsMotorsDB", null, 1);
         SQLiteDatabase bd= admin.getWritableDatabase();
+        String id = etIdMarca.getText().toString();
         String Nombre=etMarca.getText().toString();
-        ContentValues registro=new ContentValues();
-        registro.put("nombre",Nombre);
-        int cant=bd.update("marcas", registro,"nombre="+Nombre,null);
-        bd.close();
-        if(cant==1){
-            Toast.makeText(this,"Se actualizo Marca",Toast.LENGTH_SHORT).show();
+        if(id.isEmpty()){
+            Toast.makeText(this,"Ingrese un ID",Toast.LENGTH_SHORT).show();
+        }else{
+            ContentValues registro=new ContentValues();
+            registro.put("idmarcas",id);
+            registro.put("nombre",Nombre);
+            int cant=bd.update("marcas", registro,"idmarcas="+id,null);
+            bd.close();
+            if(cant==1){
+                Toast.makeText(this,"Se actualizo Marca",Toast.LENGTH_SHORT).show();
+                etMarca.setText("");
+                etIdMarca.setText("");
+            }
+            else
+                Toast.makeText(this,"No existe una marca con ese ID",Toast.LENGTH_SHORT).show();
         }
-        else
-            Toast.makeText(this,"No existe la marca ingresada",Toast.LENGTH_SHORT).show();
+        bd.close();
     }
 
     public void Borrar(View v){
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"CarsMotorsDB", null, 1);
         SQLiteDatabase bd= admin.getWritableDatabase();
         String Nombre=etMarca.getText().toString();
-        int cant=bd.delete("marcas","nombre='"+Nombre+"'",null);
-        bd.close();
-        etMarca.setText("");
-        tvCodigo.setText("");
-        if(cant==1){
-            Toast.makeText(this,"Se borra la marca Correctamente",Toast.LENGTH_SHORT).show();
+        if(Nombre.isEmpty()){
+            Toast.makeText(this,"Ingrese un nombre",Toast.LENGTH_SHORT).show();
+        }else{
+            int cant=bd.delete("marcas","nombre='"+Nombre+"'",null);
+            bd.close();
+            etMarca.setText("");
+            etIdMarca.setText("");
+            if(cant==1){
+                Toast.makeText(this,"Se borra la marca Correctamente",Toast.LENGTH_SHORT).show();
+            }
+            else
+                Toast.makeText(this,"No existe la marca",Toast.LENGTH_SHORT).show();
         }
-        else
-            Toast.makeText(this,"No existe la marca",Toast.LENGTH_SHORT).show();
+        bd.close();
     }
 }
