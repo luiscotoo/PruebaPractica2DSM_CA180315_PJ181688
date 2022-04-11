7088a48e0d2863f21f0eca9615645ea8cff467bd
Omar Pérez Jiménez <bryanomarperez2000@gmail.com>
2022-04-10 21:40:07 -0600

    Crub Vehiculos

--- CarsMotorsApp/app/src/main/java/sv/edu/udb/carsmotorsapp/CrudAutomovil.java
+++ CarsMotorsApp/app/src/main/java/sv/edu/udb/carsmotorsapp/CrudAutomovil.java
@@ -47,13 +47,13 @@ public class CrudAutomovil extends AppCompatActivity {
         comboColores=findViewById(R.id.lisColor);
         comboTipos=findViewById(R.id.lisTipo);
         ConsultarListaMarcas();
-        ArrayAdapter<CharSequence> adapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,listMarcas);
+        ArrayAdapter<CharSequence> adapter=new ArrayAdapter(this,R.layout.spinner_item,listMarcas);
         comboMarcas.setAdapter(adapter);
         ConsultarListaTipos();
-        ArrayAdapter<CharSequence> adapter1=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,listTipo);
+        ArrayAdapter<CharSequence> adapter1=new ArrayAdapter(this,R.layout.spinner_item,listTipo);
         comboTipos.setAdapter(adapter1);
         ConsultarListaColor();
-        ArrayAdapter<CharSequence> adapter2=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,listColor);
+        ArrayAdapter<CharSequence> adapter2=new ArrayAdapter(this,R.layout.spinner_item,listColor);
         comboColores.setAdapter(adapter2);
     }
 
@@ -104,7 +104,7 @@ public class CrudAutomovil extends AppCompatActivity {
 
     private void obtenerListaMarcas() {
         listMarcas=new ArrayList<String>();
-        listMarcas.add("Selecione la Marca:");
+        listMarcas.add("Selecione la Marca");
         for (int i = 0; i< instanciasList.size(); i++){
         listMarcas.add(instanciasList.get(i).getId()+"- "+ instanciasList.get(i).getNombre());
         }
@@ -112,7 +112,7 @@ public class CrudAutomovil extends AppCompatActivity {
 
     private void obtenerListaTipos() {
         listTipo=new ArrayList<String>();
-        listTipo.add("Selecione Tipo de vehiculo:");
+        listTipo.add("Selecione Tipo de vehiculo");
         for (int i = 0; i< instanciasList.size(); i++){
             listTipo.add(instanciasList.get(i).getId()+"- "+ instanciasList.get(i).getNombre());
         }
@@ -120,7 +120,7 @@ public class CrudAutomovil extends AppCompatActivity {
 
     private void obtenerListaColor() {
         listColor = new ArrayList<String>();
-        listColor.add("Selecione el Color:");
+        listColor.add("Selecione el Color");
         for (int i = 0; i < colorlist.size(); i++) {
             listColor.add(colorlist.get(i).getId() + "- " + colorlist.get(i).getNombre());
         }
@@ -172,9 +172,9 @@ public class CrudAutomovil extends AppCompatActivity {
             etPrecio.setText("");
             etImagen.setText("");
             etDescripcion.setText("");
-            comboMarcas.setId(0);
-            comboColores.setId(0);
-            comboTipos.setId(0);
+            comboMarcas.setSelection(0);
+            comboColores.setSelection(0);
+            comboTipos.setSelection(0);
             Toast.makeText(this,"Se ingreso el automovil", Toast.LENGTH_SHORT).show();
         }
         bd.close();
@@ -186,26 +186,117 @@ public class CrudAutomovil extends AppCompatActivity {
         if(modelo.isEmpty()){
             Toast.makeText(this,"Ingrese Modelo de vehiculo",Toast.LENGTH_SHORT).show();
         }else{
-            Cursor fila=bd.rawQuery("select Ma.idmarcas,Au.idautomovil, Au.modelo, Au.numero_vin, Au.numero_chasis, Au.numero_motor, Au.numero_asientos, Au.anio, Au.capacidad_asientos, Au.precio, Au.URI_IMG, Au.descripcion, cl.idcolores, Tp.idtipoautomovil from automovil Au INNER JOIN marcas Ma on Ma.idmarcas=Au.idmarcas INNER JOIN colores cl on cl.idcolores=Au.idcolores INNER JOIN tipo_automovil Tp on Tp.idtipoautomovil=Au.idtipoautomovil where Au.modelo='"+modelo+"'",null);
+            Cursor fila=bd.rawQuery("select idautomovil, modelo, numero_vin, numero_chasis, numero_motor, numero_asientos, anio, capacidad_asientos, precio, URI_IMG, descripcion,idmarcas,idtipoautomovil,idcolores from automovil where modelo='"+modelo+"'",null);
             if (fila.moveToFirst()){
-                comboMarcas.setSelection(fila.getInt(0));
-                etIdAutomovil.setText(fila.getString(1));
-                etModelo.setText(fila.getString(2));
-                etNumeroVin.setText(fila.getString(3));
-                etNumeroCha.setText(fila.getString(4));
-                etNumeroMotor.setText(fila.getString(5));
-                etNumeroAsi.setText(fila.getString(6));
-                etAnio.setText(fila.getString(7));
-                etCapacidadAsi.setText(fila.getString(8));
-                etPrecio.setText(fila.getString(9));
-                etImagen.setText(fila.getString(10));
-                etDescripcion.setText(fila.getString(11));
-                comboTipos.setSelection(fila.getInt(12));
-                comboColores.setSelection(fila.getInt(13));
-                Toast.makeText(this,"Vehiculp Encontrado con exito",Toast.LENGTH_SHORT).show();
+                etIdAutomovil.setText(fila.getString(0));
+                etModelo.setText(fila.getString(1));
+                etNumeroVin.setText(fila.getString(2));
+                etNumeroCha.setText(fila.getString(3));
+                etNumeroMotor.setText(fila.getString(4));
+                etNumeroAsi.setText(fila.getString(5));
+                etAnio.setText(fila.getString(6));
+                etCapacidadAsi.setText(fila.getString(7));
+                etPrecio.setText(fila.getString(8));
+                etImagen.setText(fila.getString(9));
+                etDescripcion.setText(fila.getString(10));
+                comboMarcas.setSelection(Integer.valueOf(fila.getString(11)));
+                comboTipos.setSelection(Integer.valueOf(fila.getString(12)));
+                comboColores.setSelection(Integer.valueOf(fila.getString(13)));
+                Toast.makeText(this,"Vehiculo Encontrado con exito",Toast.LENGTH_SHORT).show();
             }
             else
                 Toast.makeText(this,"No existe vehiculo",Toast.LENGTH_SHORT).show();
+        }
+        bd.close();
+    }
+
+    public void Actualizar(View V){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"CarsMotorsDB", null, 1);
+        SQLiteDatabase bd= admin.getWritableDatabase();
+        String id = etIdAutomovil.getText().toString();
+        String modelo=etModelo.getText().toString();
+        String numero_vin=etNumeroVin.getText().toString();
+        String numero_chasis=etNumeroCha.getText().toString();
+        String numero_motor=etNumeroMotor.getText().toString();
+        Integer numero_asientos=Integer.parseInt(etNumeroAsi.getText().toString());
+        Integer anio=Integer.parseInt(etAnio.getText().toString());
+        Integer capacidad_asientos=Integer.parseInt(etCapacidadAsi.getText().toString());
+        Double precio=Double.parseDouble(etPrecio.getText().toString());
+        String imagen=etImagen.getText().toString();
+        String descripcion=etDescripcion.getText().toString();
+        Integer idmarcas=Integer.parseInt(String.valueOf(comboMarcas.getSelectedItemPosition()));
+        Integer idtipoautomovil=Integer.parseInt(String.valueOf(comboTipos.getSelectedItemPosition()));
+        Integer idcolores=Integer.parseInt(String.valueOf(comboColores.getSelectedItemPosition()));
+        if(id.isEmpty()){
+            Toast.makeText(this,"Ingrese un ID",Toast.LENGTH_SHORT).show();
+        }else{
+            ContentValues registro=new ContentValues();
+            registro.put("modelo",modelo);
+            registro.put("numero_vin",numero_vin);
+            registro.put("numero_chasis",numero_chasis);
+            registro.put("numero_motor",numero_motor);
+            registro.put("numero_asientos",numero_asientos);
+            registro.put("anio",anio);
+            registro.put("capacidad_asientos",capacidad_asientos);
+            registro.put("precio",precio);
+            registro.put("URI_IMG",imagen);
+            registro.put("descripcion",descripcion);
+            registro.put("idmarcas",idmarcas);
+            registro.put("idtipoautomovil",idtipoautomovil);
+            registro.put("idcolores",idcolores);
+            int cant=bd.update("automovil", registro,"idautomovil="+id,null);
+            bd.close();
+            if(cant==1){
+                Toast.makeText(this,"Se actualizo el automovil",Toast.LENGTH_SHORT).show();
+                etIdAutomovil.setText("");
+                etModelo.setText("");
+                etNumeroVin.setText("");
+                etNumeroCha.setText("");
+                etNumeroMotor.setText("");
+                etNumeroAsi.setText("");
+                etAnio.setText("");
+                etCapacidadAsi.setText("");
+                etPrecio.setText("");
+                etImagen.setText("");
+                etDescripcion.setText("");
+                comboMarcas.setSelection(0);
+                comboColores.setSelection(0);
+                comboTipos.setSelection(0);
+            }
+            else
+                Toast.makeText(this,"No existe un automovil con ese ID",Toast.LENGTH_SHORT).show();
+        }
+        bd.close();
+    }
+
+    public void Borrar(View v){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"CarsMotorsDB", null, 1);
+        SQLiteDatabase bd= admin.getWritableDatabase();
+        String modelo=etModelo.getText().toString();
+        if(modelo.isEmpty()){
+            Toast.makeText(this,"Ingrese un modelo",Toast.LENGTH_SHORT).show();
+        }else{
+            int cant=bd.delete("automovil","modelo='"+modelo+"'",null);
+            bd.close();
+            etIdAutomovil.setText("");
+            etModelo.setText("");
+            etNumeroVin.setText("");
+            etNumeroCha.setText("");
+            etNumeroMotor.setText("");
+            etNumeroAsi.setText("");
+            etAnio.setText("");
+            etCapacidadAsi.setText("");
+            etPrecio.setText("");
+            etImagen.setText("");
+            etDescripcion.setText("");
+            comboMarcas.setSelection(0);
+            comboColores.setSelection(0);
+            comboTipos.setSelection(0);
+            if(cant==1){
+                Toast.makeText(this,"Se borró el automovil Correctamente",Toast.LENGTH_SHORT).show();
+            }
+            else
+                Toast.makeText(this,"No existe el automovil",Toast.LENGTH_SHORT).show();
         }
         bd.close();
     }
